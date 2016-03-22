097595f7ebb504bda02192ce7a0d401699384ef1
John Carlson <jawnnypoo@gmail.com>
2016-03-22 00:14:46 -0500

    Follow more of the renderer pattern cause it is cool

---
+++ common/src/main/java/com/commit451/springy/common/SpringyClockRenderer.java
@@ -0,0 +1,48 @@
+package com.commit451.springy.common;
+
+import android.graphics.Canvas;
+import android.text.format.Time;
+
+/**
+ * Given a canvas, renders the clock
+ */
+public class SpringyClockRenderer {
+
+    SpringyNumber mDigit1Hour;
+    SpringyNumber mDigit2Hour;
+    SpringyNumber mDigit1Minute;
+    SpringyNumber mDigit2Minute;
+    SpringyNumber mDigit1Second;
+    SpringyNumber mDigit2Second;
+
+    public SpringyClockRenderer() {
+        mDigit1Hour = new SpringyNumber(Number.ZERO);
+        mDigit2Hour = new SpringyNumber(Number.ZERO);
+        mDigit1Minute = new SpringyNumber(Number.ZERO);
+        mDigit2Minute = new SpringyNumber(Number.ZERO);
+        mDigit1Second = new SpringyNumber(Number.ZERO);
+        mDigit2Second = new SpringyNumber(Number.ZERO);
+    }
+
+    public void update(Time time) {
+        mDigit2Hour.animateTo(Number.VALUES[time.hour % 10]);
+        mDigit1Hour.animateTo(Number.VALUES[time.hour / 10]);
+        mDigit2Minute.animateTo(Number.VALUES[time.minute % 10]);
+        mDigit1Minute.animateTo(Number.VALUES[time.minute/10]);
+        mDigit2Second.animateTo(Number.VALUES[time.second % 10]);
+        mDigit1Second.animateTo(Number.VALUES[time.second/10]);
+    }
+
+    public void onDraw(Canvas canvas) {
+        int topWidth = canvas.getWidth()/4;
+        int topHeight = (int) (canvas.getHeight() * 0.66666667f);
+        int bottomWidth = canvas.getWidth()/8;
+        int bottomHeight = (int) (canvas.getHeight() * 0.333333333f);
+        mDigit1Hour.onDraw(canvas, topWidth, topHeight, 0, 0);
+        mDigit2Hour.onDraw(canvas, topWidth, topHeight, topWidth, 0);
+        mDigit1Minute.onDraw(canvas, topWidth, topHeight, 2*topWidth, 0);
+        mDigit2Minute.onDraw(canvas, topWidth, topHeight, 3*topWidth, 0);
+        mDigit1Second.onDraw(canvas, bottomWidth, bottomHeight, canvas.getWidth() - (2 * bottomWidth), topHeight);
+        mDigit2Second.onDraw(canvas, bottomWidth, bottomHeight, canvas.getWidth() - bottomWidth, topHeight);
+    }
+}
--- configapp/src/main/java/com/commit451/springy/configapp/SpringyNumberView.java
+++ configapp/src/main/java/com/commit451/springy/configapp/SpringyNumberView.java
@@ -3,29 +3,18 @@ package com.commit451.springy.configapp;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.text.format.Time;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.commit451.springy.common.*;
-import com.commit451.springy.common.Number;
+import com.commit451.springy.common.SpringyClockRenderer;
 
 /**
- * The view which shows how the time will be rendered
+ * The view which shows how the time will be rendered using the {@link com.commit451.springy.common.SpringyClockRenderer}
  */
 public class SpringyNumberView extends View {
 
-    SpringyNumber mDigit1Hour;
-    SpringyNumber mDigit2Hour;
-    SpringyNumber mDigit1Minute;
-    SpringyNumber mDigit2Minute;
-    SpringyNumber mDigit1Second;
-    SpringyNumber mDigit2Second;
-
-    Paint mPaint;
+    SpringyClockRenderer mSpringyClockRenderer;
 
     public SpringyNumberView(Context context) {
         super(context);
@@ -49,43 +38,19 @@ public class SpringyNumberView extends View {
     }
 
     private void init() {
-        mDigit1Hour = new SpringyNumber(Number.ZERO);
-        mDigit2Hour = new SpringyNumber(Number.ZERO);
-        mDigit1Minute = new SpringyNumber(Number.ZERO);
-        mDigit2Minute = new SpringyNumber(Number.ZERO);
-        mDigit1Second = new SpringyNumber(Number.ZERO);
-        mDigit2Second = new SpringyNumber(Number.ZERO);
+        mSpringyClockRenderer = new SpringyClockRenderer();
         setWillNotDraw(false);
-
-        mPaint = new Paint();
-        mPaint.setColor(Color.WHITE);
-        mPaint.setTypeface(Typeface.DEFAULT);
-        mPaint.setAntiAlias(true);
     }
 
     public void update(Time time) {
-        mDigit2Hour.animateTo(Number.VALUES[time.hour % 10]);
-        mDigit1Hour.animateTo(Number.VALUES[time.hour / 10]);
-        mDigit2Minute.animateTo(Number.VALUES[time.minute % 10]);
-        mDigit1Minute.animateTo(Number.VALUES[time.minute/10]);
-        mDigit2Second.animateTo(Number.VALUES[time.second % 10]);
-        mDigit1Second.animateTo(Number.VALUES[time.second/10]);
+        mSpringyClockRenderer.update(time);
         invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int topWidth = canvas.getWidth()/4;
-        int topHeight = (int) (canvas.getHeight() * 0.66666667f);
-        int bottomWidth = canvas.getWidth()/8;
-        int bottomHeight = (int) (canvas.getHeight() * 0.333333333f);
-        mDigit1Hour.onDraw(canvas, topWidth, topHeight, 0, 0);
-        mDigit2Hour.onDraw(canvas, topWidth, topHeight, topWidth, 0);
-        mDigit1Minute.onDraw(canvas, topWidth, topHeight, 2*topWidth, 0);
-        mDigit2Minute.onDraw(canvas, topWidth, topHeight, 3*topWidth, 0);
-        mDigit1Second.onDraw(canvas, bottomWidth, bottomHeight, canvas.getWidth() - (2 * bottomWidth), topHeight);
-        mDigit2Second.onDraw(canvas, bottomWidth, bottomHeight, canvas.getWidth() - bottomWidth, topHeight);
+        mSpringyClockRenderer.onDraw(canvas);
         postInvalidate();
     }
 }
